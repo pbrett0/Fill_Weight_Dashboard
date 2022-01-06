7bdc71327d7962d4bffdcc8b47f63cdffe008169
pbrett0 <61989314+pbrett0@users.noreply.github.com>
2022-01-06 17:37:47 +0000

    Reduce the Number of Rows

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ server = app.server
 engine_azure = create_engine("mysql+mysqlconnector://server_login:DataVisual123@data-visualisation-db.mysql.database.azure.com/data_vis_db")
 
 # Import the Excel File and assign to pd Dataframe
-df = pd.read_sql("SELECT * FROM fill_weight_data", con = engine_azure)
+df = pd.read_sql("SELECT * FROM fill_weight_data LIMIT 500", con = engine_azure)
 
 # Sorting the Values by Date-Time
 df['Measurement date-time'] = pd.to_datetime(df['Measurement date-time'])
@@ -63,7 +63,7 @@ for key in range(len(batch_stats)):
 # IPC Mode Dropdown (Note NaN will cause errors)
 dicts_1 = []
 unique_values = limit_chart['IPC Mode'].unique()
-for key in range(len(unique_values))[:-1]:
+for key in range(len(unique_values)):
     dicts_1.append({'label': str(unique_values[key]), 'value': unique_values[key]})
 
 # Dashboard Layout
